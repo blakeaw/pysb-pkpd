7949be90fce0dfa4e111be4b6de93a9a7d24f797
blakeaw <blakeaw1102@gmail.com>
2023-05-22 17:11:41 -0500

    Updated the single_dose macro to dose_bolus.

--- src/pysb/pkpd/pk_models/three_compartment.py
+++ src/pysb/pkpd/pk_models/three_compartment.py
@@ -1,6 +1,6 @@
 from pysb import Model, Compartment, Monomer, Parameter, Expression, Observable, Initial
 
-from ..macros import three_compartments, single_dose, eliminate, distribute, clearance
+from ..macros import three_compartments, dose_bolus, eliminate, distribute, clearance
 
 Model()
 
@@ -13,14 +13,14 @@ three_compartments()
 # The dose should be in units of weight, mass or moles and
 # is converted into an initial concentration by dividing the
 # amount by the compartment volume. 
-single_dose(Drug, CENTRAL, 100.)
+dose_bolus(Drug, CENTRAL, 100.)
 # Distribute drug between CENTRAL and PERIPHERAL compartments.
 distribute(Drug, CENTRAL, PERIPHERAL, [1.0, 1e-1])
 # Distribute drug between CENTRAL and DEEP_PERIPHERAL compartments.
 distribute(Drug, CENTRAL, DEEPPERIPHERAL, [1e-3, 1e-4])
 # Linear elimination of drug from the CENTRAL compartment 
 # by processes like metabolism and renal excretion.
-eliminate(Drug, CENTRAL, 1e-2)
+# eliminate(Drug, CENTRAL, 1e-2)
 # Linear elimination of the drug from the CENTRAL compartment
 # by systemic clearance. Not the clearance rate is in units of volume/time.
 clearance(Drug, CENTRAL, 10.)
--- src/pysb/pkpd/pk_models/two_compartment.py
+++ src/pysb/pkpd/pk_models/two_compartment.py
@@ -1,6 +1,6 @@
 from pysb import Model, Compartment, Monomer, Parameter, Expression, Observable, Initial
 
-from ..macros import two_compartments, single_dose, eliminate, clearance, distribute
+from ..macros import two_compartments, dose_bolus, eliminate, clearance, distribute
 
 Model()
 
@@ -14,12 +14,12 @@ two_compartments()
 # The dose should be in units of weight, mass or moles and
 # is converted into an initial concentration by dividing the
 # amount by the compartment volume. 
-single_dose(Drug, CENTRAL, 100.)
+dose_bolus(Drug, CENTRAL, 100.)
 # Distribute drug between CENTRAL and PERIPHERAL compartments.
 distribute(Drug, CENTRAL, PERIPHERAL, [1.0, 1e-1])
 # Linear elimination of drug from the CENTRAL compartment 
 # by processes like metabolism and renal excretion.
-eliminate(Drug, CENTRAL, 1e-2)
+#eliminate(Drug, CENTRAL, 1e-2)
 # Linear elimination of the drug from the CENTRAL compartment
 # by systemic clearance. Not the clearance rate is in units of volume/time.
 clearance(Drug, CENTRAL, 10.)
