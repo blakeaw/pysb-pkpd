b4bff981d18006f7fdde8a97d98b31916351efee
blakeaw <blakeaw1102@gmail.com>
2023-05-30 15:23:20 -0500

    Updated the version number from 0.1.0 to 0.2.0, as well as the PySB dependency version.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import find_namespace_packages
 
 setuptools.setup(
     name="pysb-pkpd",
-    version="0.1.0",
+    version="0.2.0",
     python_requires=">=3.8",
-    install_requires=["pysb>=1.13.2"],
+    install_requires=["pysb>=1.15.0"],
     extras_require={"cython": "cython>=0.29.25"},
     author="Blake A. Wilson",
     author_email="blakeaw1102@gmail.com",
-    description="PySB plugin package providing domain-specific macros and models for pharmacological PK/PD modeling.",
+    description="PySB add-on providing domain-specific macros and models for pharmacological PK/PD modeling.",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
     url="https://github.com/blakeaw/pysb-pkpd",
