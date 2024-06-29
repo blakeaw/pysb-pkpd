31cd60a4b9899b34ed5ad2896299f4490a0260ec
blakeaw <blakeaw1102@gmail.com>
2024-06-28 23:14:15 -0500

    Defined a new util module with the simulate function as per Issue 11. Added an import of the simulate function to the package __init__ so it can called as pkpd.simulate.

--- src/pysb/pkpd/__init__.py
+++ src/pysb/pkpd/__init__.py
@@ -3,5 +3,6 @@ from . import macros, models, pk_models
 # Go ahead and import all the macro functions into 
 # the pkpd namespace.
 from .macros import *
+from .util import simulate
 
 __version__ = '0.3.0'
---
+++ src/pysb/pkpd/util.py
@@ -0,0 +1,44 @@
+import pysb
+from pysb.simulator import ScipyOdeSimulator
+import numpy as np
+
+
+def simulate(
+    model: pysb.Model,
+    tspan: np.ndarray,
+    param_values: None | np.ndarray | list[np.ndarray] = None,
+    initials: None | np.ndarray | list[np.ndarray] = None,
+    nprocs: int = 1,
+) -> np.ndarray:
+    """Simulate the given model using the ScipyOdeSimulator.
+
+    This function abstracts setting up and running a simulation of the model using
+    the ScipyOdeSimulator with the lsoda integrator. It returns the corresponding model
+    trajectory.
+
+    Args:
+        model: The input PySB model to simulate.
+        tspan: The time span to simulate the model over.
+        param_values: Optional specification of parameters to use when
+            simulating the model. If None, the nominal/default model parameters
+            will be used. The input can be None, a single parameter vector, or
+            a list of parameter vectors that will each be simulated.
+        initials: Optional specification of initial concentrations to use when
+            simulating the model. If None, the nominal/default model values
+            will be used. The input can be None, a single vector, or
+            a list of vectors that will each be simulated.
+
+    Returns:
+        The PySB model simulation trajectory as a structured NumPy array.
+    """
+
+    simulator = ScipyOdeSimulator(
+        model,
+        tspan=tspan,
+        integrator="lsoda",
+    )
+    simulation_result = simulation.run(
+        param_values=param_values, initials=initials, num_processors=nprocs
+    )
+    simulation_trajectory = simulation_result.all
+    return simulation_trajectory
