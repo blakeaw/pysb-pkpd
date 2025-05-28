4b3f2203c1b5c8ad79673cdb889e4917f6cc1c75
blakeaw <blakeaw1102@gmail.com>
2025-05-28 15:47:28 -0500

    Completed versions of the two and three compartment model functions.

--- src/pysb/pkpd/express.py
+++ src/pysb/pkpd/express.py
@@ -175,4 +175,106 @@ def two_compartment_model(
     if pd_model is not None:
         for key, value in pd_model.items():
             PD_MODELS[key](Drug, CENTRAL, **value)
+    macros.distribute(CENTRAL, PERIPHERAL, [k12, k21])        
+    return model
+
+def three_compartment_model(
+    dose_amount: float,
+    dose_route: str = "iv-bolus",
+    volume_central: float = 1.0,
+    volume_peripheral: float = 1.0,
+    volume_deep_peripheral: float = 1.0,
+    k12: float = 1e-1,
+    k21: float = 1e-2,
+    k13: float = 1e-3,
+    k31: float = 1e-4,
+    clearance: float = 0.5,
+    pd_model: dict | None = None,
+):
+    """Generates a standard three-compartment PK/PD model.
+
+    Args:
+        dose_amount (float): The amount of drug in the dose.
+        dose_route (str, optional): The route of drug adminstration. Defaults to "iv-bolus".
+            Options: 'iv-bolus', 'iv-infusion', 'oral'.
+        volume_central (float, optional): Volume of the central compartment. Defaults to 1..
+        volume_peripheral (float, optional): Volume of the peripheral compartment. Defaults to 1..
+        k12 (float, optional): The rate constant for distribution from the cental to peripheral
+            compartment. Defaults to 0.1.
+        k21 (float, optional): The rate constant for redistribution from the peripheral to central
+            compartment. Defaults to 0.01.
+        k13 (float, optional): The rate constant for distribution from the cental to deep peripheral
+            compartment. Defaults to 0.001.
+        k31 (float, optional): The rate constant for redistribution from the deep peripheral to central
+            compartment. Defaults to 0.0001.
+        clearance (float, optional): Clearance rate of the drug (volume/time). Defaults to 0.5.
+        pd_model (dict | None, optional): Set the PD model and its paramters. Defaults to None.
+            Options: 
+                'emax' - parameters: 'emax', 'ec50'
+                'sigmoidal-emax' - parameters: 'emax', 'ec50', 'n'
+                'linear' - parameters: 'slope', 'intercept'
+                'log-linear' - parameters: 'slope', 'intercept'
+                'fixed' - parameters: 'e_fixed', 'c_threshold'
+
+    Returns:
+        pysb.Model: The model.
+
+    Examples
+    --------
+    Oral administration with Emax PD::
+
+        model = three_compartment_model(
+            100.0,  # mg
+            dose_route="oral",
+            volume_central=10.0,  # L
+            volume_peripheral=2.0, # L
+            k12=1e-2, # min^-1
+            k21=1e-4, # min^-1
+            k13=1e-3, # min^-1
+            k31=1e-5, # min^-1            
+            clearance=0.750,  # L/min
+            pd_model={
+                "emax": {
+                    "emax": 2.2,
+                    "ec50": 50.0,  # mg
+                }
+            },
+        )
+
+    I.V. infusion administration with linear PD::
+
+        model = three_compartment_model(
+            100.0,  # mg
+            dose_route="iv-infusion",
+            volume_central=10.0,  # L
+            volume_peripheral=2.0, # L
+            k12=1e-2, # min^-1
+            k21=1e-4, # min^-1
+            k13=1e-3, # min^-1
+            k31=1e-5, # min^-1    
+            clearance=0.750,  # L/min
+            pd_model={
+                "linear": {
+                    "slope": 0.2, # mg^-1
+                    "intercept": 1.2,
+                }
+            },
+        )
+    """
+    model = Model("three-compartment-model")
+    macros.drug_monomer()
+    Parameter("Vc", volume_central)
+    Parameter("Vp", volume_peripheral)
+    Parameter("Vdp", volume_deep_peripheral)
+    macros.three_compartments(c1_size=Vc, c2_size=Vp, c3_size=Vdp)
+    Parameter("dose", dose_amount)
+    DOSING_OPTIONS[dose_route](Drug, CENTRAL, dose)
+    if clearance > 0:
+        Parameter("CL", clearance)
+        macros.clearance(Drug, CENTRAL, CL)
+    if pd_model is not None:
+        for key, value in pd_model.items():
+            PD_MODELS[key](Drug, CENTRAL, **value)
+    macros.distribute(CENTRAL, PERIPHERAL, [k12, k21])
+    macros.distribute(CENTRAL, DEEP_PERIPHERAL, [k13, k31])      
     return model
